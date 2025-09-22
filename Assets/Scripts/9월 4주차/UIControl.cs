bc3c2ba053cf8469b355fbf741ca158debdabafc
KHKAC <rudgnsrb888@naver.com>
2025-09-22 11:51:30 +0900

    250922 UI 강의

---
+++ Assets/Scripts/9월 4주차/UIControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIControl : MonoBehaviour
+{
+    [SerializeField] TMP_Text titleTxt;
+    [SerializeField] Toggle toggle1;
+    [SerializeField] Slider slider;
+    [SerializeField] Button btn;
+    [SerializeField] TMP_Dropdown dropdown;
+
+    void Start()
+    {
+        titleTxt.text = "This is Title";
+    }
+
+    public void OnToggle1Clicked()
+    {
+        Debug.Log("Toggle 1 is clicked.");
+        if (toggle1.isOn)
+        {
+            Debug.Log("Toggle 1 is switched on.");
+        }
+        else
+        {
+            Debug.Log("Toggle 1 is switched off.");
+        }
+
+    }
+
+    public void OnSliderChanged()
+    {
+        Debug.Log(slider.value);
+    }
+
+    public void OnButtonClicked()
+    {
+        Debug.Log("Button is clicked");
+    }
+
+    public void OnDropdownClicked()
+    {
+        Debug.Log(dropdown.value);
+    }
+}
